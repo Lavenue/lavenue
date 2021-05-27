f87fa81766e56886b5384441b38f086f3aa75cd5
Étienne Beaulé <beauleetienne0@gmail.com>
2021-05-27 11:48:40 -0400

    Rename 'orgs' to 'organisations'

--- lavenue/organisations/apps.py
+++ lavenue/organisations/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrgsConfig(AppConfig):
+class OrganisationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orgs'
+    name = 'organisations'
