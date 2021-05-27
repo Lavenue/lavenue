c0555afdf52f6ef289b7d92d3d98df6754675d05
Étienne Beaulé <beauleetienne0@gmail.com>
2021-05-27 15:16:56 -0400

    Prefer tabs to spaces for app files
    In contrast with Django files that we shouldn't have to edit much, which
    still has 4 spaces.

--- lavenue/motions/apps.py
+++ lavenue/motions/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MotionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'motions'
+	default_auto_field = 'django.db.models.BigAutoField'
+	name = 'motions'
