d610428e8b48acac8bf0514850602eb4ce6004bb
Étienne Beaulé <beauleetienne0@gmail.com>
2021-06-06 14:23:31 -0400

    Manage static files in a folder

--- lavenue/settings.py
+++ lavenue/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'lavenue' / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'lavenue' / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
