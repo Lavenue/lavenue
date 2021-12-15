c32dd608d028d4c49d502dfb330157113d55d9c8
Étienne Beaulé <beauleetienne0@gmail.com>
2021-12-15 14:01:58 -0400

    Simplify session break search
    The break search was not working correctly, and so is replaced by a DFS
    search function that finds the first subpoint out of the session and
    moves it, adding a flag that the point was continued.

--- lavenue/organisations/views.py
+++ lavenue/organisations/views.py
@@ -82,20 +82,6 @@ class AgendaView(OrganisationMixin, TemplateView):
 
 		return root
 
-	@staticmethod
-	def find_break(session, tree, path):
-		"""Uses call by sharing with the path parameter to return the "path",
-		thus the indices of the parent nodes to the first node for a different
-		session. This is a pre-order DFS tree traversal. As we only want the
-		first, we call (and catch) an exception to break."""
-		for i, c in enumerate(tree.children):
-			if c.session_id != session:
-				path.append(i)
-				raise BreakRecursionException
-			path.append(i)
-			AgendaView.find_break(session, c, path)
-			path.pop()
-
 	def get_sessions(self):
 		"""Associate branches of the tree to specific sessions.
 
@@ -106,40 +92,32 @@ class AgendaView(OrganisationMixin, TemplateView):
 		first point to discuss, and removes that point (including children) from
 		the former."""
 		tree = self.create_point_tree()
-		sessions = Session.objects.filter(meeting=self.meeting).order_by('start')
+		sessions = Session.objects.filter(meeting__slug=self.kwargs['meeting'],
+			meeting__organisation__slug=self.kwargs['organisation']).order_by('start')
 		s_dict = {s.id: s for s in sessions}
+
+		def dfs(point, current_session, path):
+			for i, n in enumerate(point.subpoints):
+				if n.session_id != current_session:
+					n_path = deepcopy(path)
+					for p in n_path:
+						p._continued = True
+					n_point = n_path.pop(0)
+					n_path.extend(point.subpoints[i:])
+					del point.subpoints[i:]
+					n_point._children = n_path
+					s_dict[n.session_id].points.append(n_point)
+					break
+				else:
+					n_path = path.copy()
+					n_path.append(n)
+					dfs(n, current_session, n_path)
+
 		for s in sessions:
 			s.points = []
 		for n in tree:
 			s_dict[n.session_id].points.append(n)
-
-		session_order = [s.id for s in sessions]
-		for i, (pk, session) in enumerate(s_dict.items()):
-			path = []
-			if len(session.points) > 0:
-				try:
-					AgendaView.find_break(pk, session.points[-1], path)
-				except BreakRecursionException:
-					pass
-
-			"""Go through the path given and deep-copy the point in order to lop
-			subpoints that are before or after the session without affecting the
-			other."""
-			path_len = len(path)
-			if len(path) > 0 and not (len(path) == 1 and path[0] == 0):
-				pass_point = deepcopy(session.points[-1])
-				s_dict[session_order[i+1]].points.append(pass_point)
-				new_point = pass_point
-				old_point = session.points[-1]
-				for j, p in enumerate(path):
-					del new_point._children[0:p]
-					if j == path_len-1:
-						del old_point._children[p:]
-					else:
-						del old_point._children[p+1:]
-					if j < path_len-1:
-						new_point = new_point._children[0]
-						old_point = old_point._children[-1]
+			dfs(n, n.session_id, [n])
 
 		return s_dict.values()
 
