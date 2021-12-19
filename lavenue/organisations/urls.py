42f1b9c75a006c99124e4ec639432840461bb1f8
Étienne Beaulé <beauleetienne0@gmail.com>
2021-12-19 15:57:40 -0400

    Add URLs to new viewsets

--- lavenue/organisations/urls.py
+++ lavenue/organisations/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from speakers.views import ParticipantViewSet
 
-from .views import AgendaViewSet, MinutesViewSet
+from .views import AgendaViewSet, MinutesViewSet, MeetingViewSet
 
 
 urlpatterns = [
-    path('agenda/', AgendaViewSet.as_view({'get': 'retrieve'})),
-    path('minutes/', MinutesViewSet.as_view({'get': 'retrieve'})),
-    path('participants/', ParticipantViewSet.as_view({'get': 'list'})),
+	path('meeting/', MeetingViewSet.as_view({'get': 'retrieve'})),
+	path('agenda/', AgendaViewSet.as_view({'get': 'retrieve'})),
+	path('minutes/', MinutesViewSet.as_view({'get': 'retrieve'})),
+	path('participants/', ParticipantViewSet.as_view({'get': 'list'})),
 ]
--- lavenue/urls.py
+++ lavenue/urls.py
@@ -2,12 +2,15 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 
+from organisations.views import MeetingViewSet
+
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('__debug__/', include(debug_toolbar.urls)),
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 
     path('accounts/', include('users.urls')),
 
+    path('<slug:organisation>/', MeetingViewSet.as_view({'post': 'create'})),
     path('<slug:organisation>/<slug:meeting>/', include('organisations.urls')),
 ]
