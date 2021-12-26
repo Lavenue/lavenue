b9aa94ccf7c0042e67339f659114291cf2fc6a99
Étienne Beaulé <beauleetienne0@gmail.com>
2021-12-26 12:53:36 -0400

    Use better REST URLs structure

--- lavenue/organisations/urls.py
+++ lavenue/organisations/urls.py
@@ -6,7 +6,7 @@ from .views import AgendaViewSet, MinutesViewSet, MeetingViewSet
 
 
 urlpatterns = [
-	path('meeting/', MeetingViewSet.as_view({'get': 'retrieve'})),
+	path('', MeetingViewSet.as_view({'get': 'retrieve'})),
 	path('agenda/', AgendaViewSet.as_view({'get': 'retrieve'})),
 	path('minutes/', MinutesViewSet.as_view({'get': 'retrieve'})),
 	path('participants/', ParticipantViewSet.as_view({'get': 'list'})),
--- lavenue/urls.py
+++ lavenue/urls.py
@@ -1,8 +1,10 @@
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from organisations.views import MeetingViewSet
+from organisations.views import MeetingViewSet, OrganisationViewSet
 
 
 urlpatterns = [
@@ -11,6 +13,14 @@ urlpatterns = [
 
     path('accounts/', include('users.urls')),
 
-    path('<slug:organisation>/', MeetingViewSet.as_view({'post': 'create'})),
-    path('<slug:organisation>/<slug:meeting>/', include('organisations.urls')),
-]
+    path('organisations/', include([
+        path('', OrganisationViewSet.as_view({'get': 'list', 'post': 'create'})),
+        path('<slug:organisation>/', include([
+            path('', OrganisationViewSet.as_view({'get': 'retrieve'})),
+            path('meetings/', include([
+                path('', MeetingViewSet.as_view({'get': 'list', 'post': 'create'})),
+                path('<slug:meeting>/', include('organisations.urls')),
+            ])),
+        ])),
+    ])),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
